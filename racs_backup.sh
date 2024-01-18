1a16bbad9708db21027357a44bdc8174dbd3c0ef
Matt Austin <matt.austin@csiro.au>
2024-01-18 09:52:59 +0800

    Corrects a few issues (requirement for bucket to be lowercase, version missing from module load rclone). Adds more verbosity.

--- racs_backup.sh
+++ racs_backup.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --output=slurm-RACS-BACKUP-AUTO_JID-%j.out
 #SBATCH --partition=copy
-#SBATCH --time=00:30:00
+#SBATCH --time=04:00:00
 set -e
 
 
@@ -48,6 +48,7 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+: ${SBID?Need a value}
 
 ## Parse arguments
 ### Second argument is CAL_SBID
@@ -59,13 +60,13 @@ DATA_DIR=${3:-"/askapbuffer/scott/askap-scheduling-blocks/${SBID}"}
 
 
 ## Load up appropriate modules
-module load rclone
+module load rclone/1.62.2
 
 ## Rclone config
 # Assuming the remote is called 'askap' 
 #   - need to ensure this is in the rclone config
 # Here we're also locking in the bucket name to be 'RACSlow3-backup'
-BUCKET_NAME="RACSlow3-backup"
+BUCKET_NAME="racslow3-backup"
 REMOTE_NAME="askap"
 
 
@@ -77,17 +78,23 @@ rclone mkdir ${REMOTE_NAME}:${BUCKET_NAME}
 # TODO: Decide on 'copy' or 'move'
 # The latter will delete the data from disk on success
 ## Use pigz for compression
+echo "Tarring ${DATA_DIR}..."
 tar \
     --use-compress-program="pigz --best --recursive" \
     -cf ${SBID}.tar.gz \
-    ${DATA_DIR} && \
+    ${DATA_DIR}
+
+echo "Uploading ${SBID}.tar.gz..."
 rclone \
     copy \
     -P \
     ${SBID}.tar.gz \
     ${REMOTE_NAME}:${BUCKET_NAME}/
 
+echo "Removing ${SBID}.tar.gz..."
+rm -v ${SBID}.tar.gz
 
+echo "Emitting racs.backup_completed CLINK event..."
 # Emit racs.backup_completed CLINK event, with scheduling block payload
 clink emit-event --input - <<EOF
 {
